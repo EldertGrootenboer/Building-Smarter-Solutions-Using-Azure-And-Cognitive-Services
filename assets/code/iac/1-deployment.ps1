f860a6ed7683639093543ddc54466c2368b661c8
Eldert Grootenboer <eldert@eldert.net>
2020-09-24 17:14:49 +0200

    Further automation of deployment

--- assets/code/iac/1-deployment.ps1
+++ assets/code/iac/1-deployment.ps1
@@ -2,9 +2,15 @@
 #   1. Create a resource group
 #   2. Deploy Azure services
 
+########## Set before deployment, do not save to repository ##########
+# If left empty, the corresponding secret will not be updated
+$rdwApiSubscriptionKey = ConvertTo-SecureString "" -AsPlainText -Force
+######################################################################
+
 # Update these according to the environment
 $subscriptionName = "Visual Studio Enterprise"
 $resourceGroupName = "rg-building-smarter-solutions-using-cognitive-services"
+$appRegistrationName = "sp-building-smarter-solutions-using-cognitive-services"
 $administratorEmail = "me@eldert.net"
 $basePath = "C:\Users\elder\OneDrive\Sessions\Building-Smarter-Solutions-Using-Azure-And-Cognitive-Services"
 
@@ -14,9 +20,24 @@ Get-AzSubscription -SubscriptionName $subscriptionName | Set-AzContext
 # Retrieves the dynamic parameters
 $administratorObjectId = (Get-AzADUser -Mail $administratorEmail).Id
 
+# If the app registration doesn't exist, we will create one
+$appRegistration = Get-AzADApplication -DisplayName $appRegistrationName
+if(-not $appRegistration)
+{
+    # Create app registration
+    $appRegistration = New-AzADApplication -DisplayName $appRegistrationName -IdentifierUris "http://$appRegistrationName"
+
+    # Create client secret
+    $bytes = New-Object Byte[] 32
+    ([System.Security.Cryptography.RandomNumberGenerator]::Create()).GetBytes($bytes)
+    $clientSecret = [System.Convert]::ToBase64String($bytes) | ConvertTo-SecureString -AsPlainText -Force
+    $endDate = [System.DateTime]::Now.AddYears(5)
+    New-AzADAppCredential -ObjectId $appRegistration.ObjectId -Password $clientSecret -EndDate $endDate
+}
+
 # Create the resource group and deploy the resources
 New-AzResourceGroup -Name $resourceGroupName -Location 'West Europe' -Tag @{CreationDate=[DateTime]::UtcNow.ToString(); Project="Building Smarter Solutions Using Azure and Cognitive Services"; Purpose="Session"}
-New-AzResourceGroupDeployment -Name "BuildSmarterSolutions1" -ResourceGroupName $resourceGroupName -TemplateFile "$basePath\assets\code\iac\azuredeploy.1.json" -administratorObjectId $administratorObjectId
+New-AzResourceGroupDeployment -Name "BuildSmarterSolutions1" -ResourceGroupName $resourceGroupName -TemplateFile "$basePath\assets\code\iac\azuredeploy.1.json" -administratorObjectId $administratorObjectId -rdwApiSubscriptionKey $rdwApiSubscriptionKey -servicePrincipalPasswordEventGridValue $clientSecret
 
 # Deploy the Bot
 Invoke-Expression "$basePath\assets\code\bot\Scripts\deployment.ps1"
